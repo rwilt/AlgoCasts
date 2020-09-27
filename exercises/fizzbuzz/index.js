2be60ba261ed79892603c20665b99031506b5587
rwilt <rosie.wilt@gmail.com>
2020-09-27 19:09:46 -0400

    practice

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+for (let i=1; i <= n; i++){
+
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log("fizzbuzz")
+    } else if (n % 3 === 0){
+        console.log("fizz")
+    }
+    else if (n % 5 === 0){
+        console.log("buzz")
+    }
+
+}
+
+
+}
 
 module.exports = fizzBuzz;
